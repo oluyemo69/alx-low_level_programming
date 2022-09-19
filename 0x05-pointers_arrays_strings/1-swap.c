903935e4add36be1e573c295af206687babb8a11
oluyemo69 <111234942+oluyemo69@users.noreply.github.com>
2022-09-19 21:59:09 +0100

    Task 1

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,1 +1,16 @@
-f
+#include "main.h"
+
+/**
+ * swap_int - swaps the value of varaibles
+ * @a: first variable
+ * @b: second variable
+ * Return: Always 0.
+ */
+
+void swap_int(int *a, int *b)
+{
+	int c = *a;
+	int d = *b;
+	*a = d;
+	*b  = c;
+}
